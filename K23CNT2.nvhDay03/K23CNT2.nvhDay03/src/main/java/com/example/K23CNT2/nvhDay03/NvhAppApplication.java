d452df29d81cb4e58596425cc9b83408e6b2b737
N-V-Hung <nongvanhungcb2k5@gmail.com>
2025-11-10 15:33:48 +0700

    Day 03

---
+++ K23CNT2.nvhDay03/K23CNT2.nvhDay03/src/main/java/com/example/K23CNT2/nvhDay03/NvhAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.K23CNT2.nvhDay03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvhAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvhAppApplication.class, args);
+	}
+
+}
---
+++ K23CNT2.nvhDay03/K23CNT2.nvhDay03/src/test/java/com/example/K23CNT2/nvhDay03/NvhAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.K23CNT2.nvhDay03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvhAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
